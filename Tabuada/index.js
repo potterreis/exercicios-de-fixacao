cbdd12606b1ad51fea38634c802974f2f14515ff
Pedro Reis <zmonkeigod@gmail.com>
2024-10-11 16:36:52 -0300

    Exercicio de Tabuada usando FOR

---
+++ Tabuada/index.js
@@ -0,0 +1,10 @@
+// Faça um programa que mostre as tabuadas dos números de 1 a 10 usando laços de repetição.
+
+let tabuada = 0
+
+for(let i = tabuada; i <= tabuada + 10; i++ ){
+    console.log("Tabuada do numero: " + i)
+    for(let tabCont = 0; tabCont <= 10; tabCont++){
+        console.log(i + " * " + tabCont + " = " + (i * tabCont))
+    }
+}
